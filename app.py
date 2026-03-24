0a397cb5d0beaf4ee304a5fea5c2af88b6f916a7
amanalex2006 <poothicoteaman@gmail.com>
2026-03-24 23:19:28 +0530

    1st

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from flask import Flask
-from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+from sqlalchemy import text
 
 load_dotenv()
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "connect_args": {"sslmode": "require"}
+}
+
+db = SQLAlchemy(app)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    return "Blood Bank App Running"
+
+
+@app.route("/test-db")
+def test_db():
+    try:
+        result = db.session.execute(text("SELECT 1"))
+        return "Database Connected Successfully ✅"
+    except Exception as e:
+        return f"Connection Failed ❌<br>{str(e)}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
